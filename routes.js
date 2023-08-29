81b1a19089ce2ff102fa11af4bfc6716dd9d24ff
Fernando Roa <froao@unal.edu.co>
2023-08-29 16:07:02 -0300

    feat: show data from .js

--- routes.js
+++ routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const routes  = express.Router()
 
+const teachers = require("./data")
+
 routes.get("/", function(req, res){
 	return res.redirect("/teachers")
 })
 
 routes.get("/teachers", function(req, res){
-	return res.render("teachers/index")
+	return res.render("teachers/index", {items:teachers} )
 })
 
 routes.get("/students", function(req, res){
